74c90b924987b9c4cdb9bf3b6b1d486fcbb7ce5e
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-03-02 18:21:11 +0000

    Add styles to navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "../styles/NavBar.module.css";
-
 import logo from "../assets/logo.png";
 
 import { Navbar } from "react-bootstrap";
@@ -11,13 +10,13 @@ const NavBar = () => {
     
     return (
         <Navbar className={styles.NavBar} fixed="top">
-            <Container>
-                <Navbar.Brand>
+            <Container fluid>
+                <Navbar.Brand className="pl-5">
                     <img src={logo} alt="logo" height="150" />
                 </Navbar.Brand>
         
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav" className="pr-5">
                     <Nav className="ml-auto text-left">
                         <Nav.Link>
                             <i className="fas fa-home"></i>Home
