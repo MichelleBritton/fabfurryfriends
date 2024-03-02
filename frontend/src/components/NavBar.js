348f2fe43b1ee8b0ce3e8ce3f0001b53635ac1c1
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-03-02 17:33:40 +0000

    Add basic navigation

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,33 @@
 import React from "react";
 import styles from "../styles/NavBar.module.css";
 
+import logo from "../assets/logo.png";
+
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+
 const NavBar = () => {
     
     return (
-        <div>Test</div>
-  )
+        <Navbar className={styles.NavBar} fixed="top">
+            <Container>
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" height="150" />
+                </Navbar.Brand>
+        
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-left">
+                        <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
+                        <Nav.Link><i className="fas fa-home"></i>Our Dogs</Nav.Link>
+                        <Nav.Link><i className="fas fa-home"></i>Sign Up</Nav.Link>
+                        <Nav.Link><i className="fas fa-home"></i>Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
 }
 
 export default NavBar;
