fe2bc4f8a7fd105f99df731068d59a577eeb35d6
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-03-04 15:08:57 +0000

    Add Profile List and Detail Views, serializer and add urls

---
+++ profiles/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """ 
+    ProfileSerializer class which inherits from ModelSerializer
+    Specify owner as a readonly field so it can't be edited
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'email', 'phone', 'address', 'marital_status', 'age', 
+            'children', 'children_age', 'daily_life', 'other_pets', 
+            'describe_house', 'describe_garden', 'home_status', 
+            'where_dog_live', 'dog_left_alone', 'previously_owned', 'why', 
+            'sex', 'preferred_age', 'when', 'activities', 'image', 'is_owner', 
+        ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+    #permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
