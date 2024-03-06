85c9befee1294956392b2a1e1d849cea4751cd54
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-03-06 16:31:39 +0000

    Add AdvertCreate class to view

--- adverts/views.py
+++ adverts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from fabfurryfriends.permissions import IsOwnerOrAdmin
+from .models import Advert
+from .serializers import AdvertSerializer
 
-# Create your views here.
+
+class AdvertCreate(generics.CreateAPIView):
+    """
+    Create an advert if logged in admin user
+    The perform_create method associates the post with the logged in admin user.
+    """
+    serializer_class = AdvertSerializer
+    permission_classes = [IsOwnerOrAdmin]
+    queryset = Advert.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
