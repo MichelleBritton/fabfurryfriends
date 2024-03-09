2f8d000d6ab0c75faa41396aefb8558849303ef4
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-03-09 19:47:39 +0000

    Edit styles for intro panel

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -13,8 +13,8 @@ const Home = () => {
         <Row>
             <Col className={`${styles.ImgContainer} p-0 position-relative`} xs={12}>
                 <img src={mainImg} alt="Boxer Dog" className={styles.Image} />
-                <div className={`${styles.Panel} position-absolute p-5`}>
-                    <h1 className='display-3 mb-5'>Fab Furry Friends Dog Rescue</h1>
+                <div className={`${styles.Panel} position-absolute p-2 p-lg-5`}>
+                    <h1 className='mb-2 lg-mb-5'>Fab Furry Friends Dog Rescue</h1>
                     <p>
                         Every year, we take in over 300 dogs for a variety of reasons.
                         Our qualified & experienced team will help you find your new 
