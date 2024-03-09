413ca595280d58be7eb0c9a0ad5ec4e39d279246
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-03-09 19:28:00 +0000

    Add introduction panel

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -11,8 +11,16 @@ const Home = () => {
   return (
     <Container fluid className="p-0">
         <Row>
-            <Col className="p-0" xs={12}>
+            <Col className={`${styles.ImgContainer} p-0 position-relative`} xs={12}>
                 <img src={mainImg} alt="Boxer Dog" className={styles.Image} />
+                <div className={`${styles.Panel} position-absolute p-5`}>
+                    <h1 className='display-3 mb-5'>Fab Furry Friends Dog Rescue</h1>
+                    <p>
+                        Every year, we take in over 300 dogs for a variety of reasons.
+                        Our qualified & experienced team will help you find your new 
+                        best friend & a great match for your home, family & lifestyle.
+                    </p>
+                </div>
             </Col>
         </Row>
     </Container>
