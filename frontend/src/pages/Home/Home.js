3dcd582c70211bb9b9a63ca62b82afe70c52e46a
Michelle Britton <michelle@daisy-webdesign.co.uk>
2024-03-11 14:40:10 +0000

    Show four adverts on homepage

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/Home.module.css";
-
 import mainImg from "../../assets/main-image.webp";
+import NoResults from "../../assets/no-results.png";
 
 import { Container } from "react-bootstrap";
 import { Row } from "react-bootstrap";
@@ -11,33 +11,52 @@ import { Col } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
 
-const Home = () => {
-  return (
-    <Container fluid className="p-0">
-        <Row>
-            <Col className={`${styles.ImgContainer} p-0 position-relative`} xs={12}>
-                <img src={mainImg} alt="Boxer Dog" className={styles.Image} />
-                <div className={`${styles.Panel} position-absolute p-2 p-lg-5`}>
-                    <h1 className="mb-2 lg-mb-5">Fab Furry Friends Dog Rescue</h1>
-                    <p>
-                        Every year, we take in over 300 dogs for a variety of reasons.
-                        Our qualified & experienced team will help you find your new 
-                        best friend & a great match for your home, family & lifestyle.
-                    </p>
-                </div>
-            </Col>
-        </Row>
-        <Row>
-            <Col className="text-center py-5">
-                <h2 className={appStyles.Red}>We are looking for homes...</h2>
+import { useAdvertData, useSetAdvertData } from "../../contexts/AdvertsContext";
+import Advert from "../adverts/Advert";
+import Asset from "../../components/Asset";
 
-                <Link to={"/adverts/"}>
-                    View all
-                </Link>
-            </Col>            
-        </Row>
-    </Container>
-  );
+function Home ({ message }) {
+    const advertData = useAdvertData(); 
+    const setAdverts = useSetAdvertData(); 
+
+    return (
+        <Container fluid className="p-0">
+            <Row>
+                <Col className={`${styles.ImgContainer} p-0 position-relative`} xs={12}>
+                    <img src={mainImg} alt="Boxer Dog" className={styles.Image} />
+                    <div className={`${styles.Panel} position-absolute p-2 p-lg-5`}>
+                        <h1 className="mb-2 lg-mb-5">Fab Furry Friends Dog Rescue</h1>
+                        <p>
+                            Every year, we take in over 300 dogs for a variety of reasons.
+                            Our qualified & experienced team will help you find your new 
+                            best friend & a great match for your home, family & lifestyle.
+                        </p>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="text-center py-5">
+                    <h2 className={appStyles.Red}>We are looking for homes...</h2>
+
+                    {advertData.results.length ? (
+                        advertData.results.slice(0,4).map((advert) => (  
+                            <Col key={advert.id} className={styles.Card} xs={12} md={6} xl={4}>
+                                <Advert {...advert} setAdverts={setAdverts} /> 
+                            </Col>                       
+                        ))
+                    ) : (
+                        <Container className={appStyles.Content}>
+                            <Asset src={NoResults} message={message} />
+                        </Container>
+                    )}
+
+                    <Link to={"/adverts/"}>
+                        View all
+                    </Link>
+                </Col>            
+            </Row>
+        </Container>
+    );
 }
 
 export default Home;
